3da6a24d6e872eb9758e0ac25b80ed5414e7b63a
Brendon Tse and Loewy Malkovich <loewy@>
2019-09-11 11:06:07 -0700

    add dropdown menu to html and API key to .env

---
+++ src/cats-interface.js
@@ -0,0 +1,29 @@
+import $ from 'jquery';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+$(document).ready(function() {
+  $('#weatherLocation').click(function() {
+    const city = $('#location').val();
+    $('#location').val("");
+
+    let request = new XMLHttpRequest();
+    const url = `https://ghibliapi.herokuapp.com/species/${process.env.CATS_KEY}`
+
+    request.onreadystatechange = function() {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    }
+
+    request.open("GET", url, true);
+    request.send();
+
+    const getElements = function(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The weather is ${response.weather[0].main} forever.`);
+    }
+  });
+});
